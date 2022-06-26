2c8437eb723e3387b9ca88b753df4a2a02a42825
yuness <yunessaga@gmail.com>
2022-06-26 13:52:54 +0100

    middleware

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ MongoClient.connect(dbConnectionString)
         collection = db.collection('test1')
     })
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+
+
 app.listen(process.env.PORT || PORT, () => {
     console.log(`server is running on port ${PORT}`)
 })
